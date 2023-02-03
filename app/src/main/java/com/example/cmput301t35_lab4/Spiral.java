6e67fe51ff190e3bd636f3d3f4db5034b5ed7e6c
MingbaoF <mingbao@ualberta.ca>
2023-02-02 23:28:33 -0700

    Y Be Bubble

---
+++ app/src/main/java/com/example/cmput301t35_lab4/Spiral.java
@@ -0,0 +1,13 @@
+package com.example.cmput301t35_lab4;
+
+public class Spiral extends Shape {
+	private int length;
+	private int radius;
+	
+	public Spiral(int x, int y, int length, int radius) {
+		super(x, y)
+		this.length = length;
+		this.radius = radius;
+	}
+
+}
