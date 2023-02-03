0deffce326d3b1ba7458cf1c3f0d2790f0225cbd
Edward7n15 <90708301+Edward7n15@users.noreply.github.com>
2023-02-03 01:09:25 -0700

    Update Shape.java

--- app/src/main/java/com/example/cmput301t35_lab4/Shape.java
+++ app/src/main/java/com/example/cmput301t35_lab4/Shape.java
@@ -4,6 +4,13 @@ package com.example.cmput301t35_lab4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+
+
+    String color = " blue";
+
+=======
+    //String color = "black";
 
     // create a constructor for Shape
     public Shape(int x, int y) {
