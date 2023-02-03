a1b3b2099ccd1bb9d61c759c339312b3fc018eef
JohnsonWu1 <wjc2848949342@gmail.com>
2023-02-02 18:36:13 -0700

    Shape.java added

---
+++ app/src/main/java/com/example/cmput301t35_lab4/Shape.java
@@ -0,0 +1,26 @@
+package com.example.cmput301t35_lab4;
+
+// create an abstract class called Shape with x and y integer fields
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    // create a constructor for Shape
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // create a getter for x
+    public int getX() {
+        return x;
+    }
+
+    // create a getter for y
+    public int getY() {
+        return y;
+    }
+
+    // create an abstract method called area
+    public abstract double area();
+}
