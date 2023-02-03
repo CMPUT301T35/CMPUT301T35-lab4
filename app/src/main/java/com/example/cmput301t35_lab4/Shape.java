5aa0079d795d5e907a7f4ad6d35efd33e3571c81
ganfanlen <97862947+ganfanlen@users.noreply.github.com>
2023-02-03 00:01:09 -0700

    Update Shape.java

--- app/src/main/java/com/example/cmput301t35_lab4/Shape.java
+++ app/src/main/java/com/example/cmput301t35_lab4/Shape.java
@@ -5,6 +5,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
+    String color = "black";
     // create a constructor for Shape
     public Shape(int x, int y) {
         this.x = x;
